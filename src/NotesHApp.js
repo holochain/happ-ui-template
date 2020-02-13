30af0c758ac39b75becea31cb5438d45fce53224
Robbie Carlton <robbie.carlton@gmail.com>
2020-02-13 12:30:33 -0700

    add note-list

--- src/NotesHApp.js
+++ src/NotesHApp.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import LIST_NOTES_QUERY from './graphql/listNotesQuery'
 import './NotesHApp.css'
 
 function NotesHApp() {
-  return <div>
-    Notes hApp
+  const { data: { listNotes } = { listNotes: [] } } = useQuery(LIST_NOTES_QUERY)
+
+  return <div className='notes-happ'>
+    <h1>Notes hApp</h1>
+    <div className='note-list'>
+      {listNotes.map(note => 
+        <NoteRow note={note} key={note.id} />)}
+    </div>
+  </div>
+}
+
+function NoteRow({ note: { title, content } }) {
+  return <div className='note-row'>
+    <h3>{title}</h3>
+    <div>{content}</div>
   </div>
 }
 
 export default NotesHApp
+
