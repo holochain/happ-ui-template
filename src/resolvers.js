d64b33bac9a1fcea2b171e009c392753b3db9f39
Lisa <ldjetton21@gmail.com>
2020-02-18 18:37:42 -0600

    cleaned

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,7 +1,6 @@
 import { createZomeCall } from './holochainClient'
 
 function dnaToUiNote (noteResult) {
-  console.log('noteResult : ', noteResult)
   return {
     ...noteResult,
     createdAt: noteResult.created_at
@@ -18,13 +17,11 @@ export const resolvers = {
   },
 
   Mutation: {
-    createNote: async (_, { noteInput }) => dnaToUiNote(await createZomeCall('/notes/notes/create_note')({ note_input: noteInput })),
+    createNote: async (_, { noteInput }) => 
+      dnaToUiNote(await createZomeCall('/notes/notes/create_note')({ note_input: noteInput })),
 
-    updateNote: async (_, { id, noteInput }) => {
-      console.log('noteInput : ', noteInput)
-      console.log('id : ', id)
-      dnaToUiNote(await createZomeCall('/notes/notes/update_note')({ id, note_input: noteInput }))
-    },
+    updateNote: async (_, { id, noteInput }) =>
+      dnaToUiNote(await createZomeCall('/notes/notes/update_note')({ id, note_input: noteInput })),
 
     removeNote: async (_, { id }) =>
       dnaToUiNote(await createZomeCall('/notes/notes/remove_note')({ id }))
